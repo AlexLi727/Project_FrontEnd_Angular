c715f5e91748d2913dade0c02dad57f388903a83
kaimex9 <kaimexalabau@gmail.com>
2024-12-02 00:27:09 +0100

    functionalProject

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ListNursesComponent } from './list-nurses/list-nurses.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { FindByNameComponent } from './find-by-name/find-by-name.component';
 
 export const routes: Routes = [
   { path: 'list-nurses', component: ListNursesComponent },
+  { path: 'find-by-name', component:  FindByNameComponent},
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
 ];
