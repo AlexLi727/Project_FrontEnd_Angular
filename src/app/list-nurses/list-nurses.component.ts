a31aa4bd7a291b22a69ccfa5aa84e1b0d27224ed
Jonathan <canojonathanbenavides@gmail.com>
2024-12-12 17:27:46 +0100

    ASDF

--- src/app/list-nurses/list-nurses.component.ts
+++ src/app/list-nurses/list-nurses.component.ts
@@ -1,5 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { NursesServiceService } from '../../services/nurses-service.service';
 
 @Component({
   selector: 'app-list-nurses',
@@ -9,18 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './list-nurses.component.css'
 })
 export class ListNursesComponent {
-  nurses = {
-    "nurses": [
-      { "id": 1, "first_name": "Guillemette", "last_name": "Yole" },
-      { "id": 2, "first_name": "Sylvester", "last_name": "Minnock" },
-      { "id": 3, "first_name": "Jennifer", "last_name": "Miranda" },
-      { "id": 4, "first_name": "Malory", "last_name": "Kingsly" },
-      { "id": 5, "first_name": "Bowie", "last_name": "Fitzsimmons" },
-      { "id": 6, "first_name": "Gothart", "last_name": "Brattell" },
-      { "id": 7, "first_name": "Brockie", "last_name": "Rookes" },
-      { "id": 8, "first_name": "Tiff", "last_name": "Liddell" },
-      { "id": 9, "first_name": "Carine", "last_name": "Whenman" },
-      { "id": 10, "first_name": "Suellen", "last_name": "Jenkerson" }
-    ]
-  };
+  
 }
